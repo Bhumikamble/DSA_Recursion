d8b2f07cdbf9cd2c547b60997f234c542be44140
Bhumikamble <bhumikamble714@gmail.com>
2025-03-17 21:42:57 +0530

    .

--- Patterns/Pyramid.java
+++ Patterns/Pyramid.java
@@ -1,12 +1,18 @@
-public class Rectangle {
+
+
+public class Pyramid {
     public static void main(String args[]){
         for(int i=1;i<=4;i++){
-            for(int j=0;j<=5;j++){
+            for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 }
-    
 
+//output
+//* 
+//** 
+//*** 
+//**** 
