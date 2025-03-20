d9d01c3d416ffc786f4e7ee0dd753b2ef3bc1165
Bhumikamble <bhumikamble714@gmail.com>
2025-03-20 22:17:31 +0530

    .

---
+++ Patterns/Butterfly.java
@@ -0,0 +1,41 @@
+public class Butterfly {
+    public static void main(String [] args){
+        int n=4;
+        for(int i=1;i<=n;i++){
+            //1st part of butterfly
+            //upper-half
+            for(int j=1;j<=i;j++){
+                System.out.print("*");  
+            }
+
+            //spaces
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+
+            //2nd part of butterfly
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //
+        for(int i=n;i>=1;i--){
+            //3rd part of butterfly
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            //spaces
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+
+            //2nd part of butterfly
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ Patterns/FloydsTriangle.java
@@ -0,0 +1,13 @@
+public class FloydsTriangle {
+    public static void main(String[] args){
+        int i=5,k=1;
+        int n;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(k);
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
