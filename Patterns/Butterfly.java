bbf770fa27ed3c68f8ef8c39902b785819db790f
Bhumikamble <bhumikamble714@gmail.com>
2025-03-21 20:46:46 +0530

    .

--- Patterns/Butterfly.java
+++ Patterns/Butterfly.java
@@ -2,8 +2,10 @@ public class Butterfly {
     public static void main(String [] args){
         int n=4;
         for(int i=1;i<=n;i++){
+            
             //1st part of butterfly
             //upper-half
+
             for(int j=1;j<=i;j++){
                 System.out.print("*");  
             }
